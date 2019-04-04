f7ad58b499812b0f40bf1e492fb9fc076c4aa9ca
Sergi Palau <sergi.palau@forcemanager.net>
2019-04-04 09:44:27 +0200

    fix version mistyping

--- src/bridge.js
+++ src/bridge.js
@@ -1,50 +1,50 @@
 import postRobot from 'post-robot';
 
 const packageJson = require('../package.json');
-const veresion = packageJson.version.substring(0, packageJson.version.indexOf('.'));
+const version = packageJson.version.substring(0, packageJson.version.indexOf('.'));
 const guid = window.name;
 
 const client = {
   getEntityId() {
-    return postRobot.sendToParent('getEntityId', { veresion });
+    return postRobot.sendToParent('getEntityId', { version });
   },
 
   getToken() {
-    return postRobot.sendToParent('getToken', { veresion, guid });
+    return postRobot.sendToParent('getToken', { version, guid });
   },
 
   getNewToken() {
-    return postRobot.sendToParent('getNewToken', { veresion, guid });
+    return postRobot.sendToParent('getNewToken', { version, guid });
   },
 
   getLiteral(literal) {
-    if (!literal) return Promise.reject('No literal');
-    return postRobot.sendToParent('getLiteral', { veresion, literal });
+    if (!literal) return Promise.reject({ msg: 'No literal' });
+    return postRobot.sendToParent('getLiteral', { version, literal });
   },
 
   getCultureLang() {
-    return postRobot.sendToParent('getCultureLang', { veresion });
+    return postRobot.sendToParent('getCultureLang', { version });
   },
 
   getUserLocale() {
-    return postRobot.sendToParent('getUserLocale', { veresion });
+    return postRobot.sendToParent('getUserLocale', { version });
   },
 
   getUserId() {
-    return postRobot.sendToParent('getUserId', { veresion });
+    return postRobot.sendToParent('getUserId', { version });
   },
 
   getFilteredUsers() {
-    return postRobot.sendToParent('getFilteredUsers', { veresion });
+    return postRobot.sendToParent('getFilteredUsers', { version });
   },
 
   getPermissions() {
-    return postRobot.sendToParent('getPermissions', { veresion });
+    return postRobot.sendToParent('getPermissions', { version });
   },
 
   setDrilldown(key, value) {
-    if (!key || !value) return Promise.reject('No key or value');
-    return postRobot.sendToParent('setDrilldown', { veresion, key, value });
+    if (!key || !value) return Promise.reject({ msg: 'No key or value' });
+    return postRobot.sendToParent('setDrilldown', { version, key, value });
   },
 
   getFilteredPeriodString(period) {
@@ -54,7 +54,7 @@ const client = {
         endDate: period.dateEnd.getTime(),
       };
     }
-    return postRobot.sendToParent('getFilteredPeriodString', { veresion });
+    return postRobot.sendToParent('getFilteredPeriodString', { version });
   },
 
   // ##### FORM FUNCTIONS ##### //
@@ -62,14 +62,14 @@ const client = {
   getFormInitData() {
     console.log('fm-bridge getFormInitData');
     return postRobot
-      .sendToParent('getFormInitData', { veresion, guid })
+      .sendToParent('getFormInitData', { version, guid })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   getValueList(tableName) {
     return postRobot
-      .sendToParent('getValueList', { veresion, tableName })
+      .sendToParent('getValueList', { version, tableName })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
@@ -80,105 +80,105 @@ const client = {
 
   getRelatedEntitiesById(idEntityIn, id, idEntityOut) {
     return postRobot
-      .sendToParent('getUsuarios', { veresion, idEntityIn, id, idEntityOut })
+      .sendToParent('getUsuarios', { version, idEntityIn, id, idEntityOut })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   getFormType(idTipoForm) {
     return postRobot
-      .sendToParent('getFormType', { veresion, idTipoForm })
+      .sendToParent('getFormType', { version, idTipoForm })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   collapseImagesView() {
     return postRobot
-      .sendToParent('collapseImagesView', { veresion })
+      .sendToParent('collapseImagesView', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   expandImagesView() {
     return postRobot
-      .sendToParent('expandImagesView', { veresion })
+      .sendToParent('expandImagesView', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   finishActivity() {
     return postRobot
-      .sendToParent('finishActivity', { veresion })
+      .sendToParent('finishActivity', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   setTitle(title) {
     return postRobot
-      .sendToParent('setTitle', { veresion, title })
+      .sendToParent('setTitle', { version, title })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   saveData(json) {
     return postRobot
-      .sendToParent('saveData', { veresion, json })
+      .sendToParent('saveData', { version, json })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   openDialogPicker(eventName, date, dateMax = '', dateMin = '') {
     return postRobot
-      .sendToParent('openDialogPicker', { veresion, eventName, date, dateMax, dateMin })
+      .sendToParent('openDialogPicker', { version, eventName, date, dateMax, dateMin })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   openSignatureView(id, background = 'white') {
     return postRobot
-      .sendToParent('openSignatureView', { veresion, id, background })
+      .sendToParent('openSignatureView', { version, id, background })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   showCameraImages() {
     return postRobot
-      .sendToParent('showCameraImages', { veresion })
+      .sendToParent('showCameraImages', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   hideCameraImages() {
     return postRobot
-      .sendToParent('hideCameraImages', { veresion })
+      .sendToParent('hideCameraImages', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   showLoading() {
     return postRobot
-      .sendToParent('showLoading', { veresion })
+      .sendToParent('showLoading', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   hideLoading() {
     return postRobot
-      .sendToParent('hideLoading', { veresion })
+      .sendToParent('hideLoading', { version })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   showAlertDialog(id, message, btnOk) {
     return postRobot
-      .sendToParent('showAlertDialog', { veresion, id, message, btnOk })
+      .sendToParent('showAlertDialog', { version, id, message, btnOk })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
 
   showConfirmDialog(id, message, btnOkStr, btnKOStr) {
     return postRobot
-      .sendToParent('showConfirmDialog', { veresion, id, message, btnOkStr, btnKOStr })
+      .sendToParent('showConfirmDialog', { version, id, message, btnOkStr, btnKOStr })
       .then((res) => res.data)
       .catch((err) => Promise.reject(err));
   },
